2fb512102fc158aedc6fc05b7f6191d88e1b7e74
Maria Vitoria <vitmachadov@gmail.com>
2026-06-26 07:54:04 -0300

    Fraude Detection API

---
+++ src/test/java/com/vitoria/ai/frauddetection/FraudDetectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitoria.ai.frauddetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
